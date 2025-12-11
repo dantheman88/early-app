00ed2e372301804a86377cf0277777c1bec68534
dantheman88 <dannymcmullan6@gmail.com>
2025-12-11 18:18:33 +0000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to Early App</h1>
+      <p>Your deployment is working!</p>
+    </div>
+  );
+}
