4fd98907b85ad61b80b2351cfe5a9490399d5bb2
dantheman88 <dannymcmullan6@gmail.com>
2025-12-11 17:32:30 +0000

    Delete pages directory

--- pages/api/admin/create-offer.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    id: Math.random().toString(36).substring(7),
-    message: 'Offer created (placeholder)'
-  });
-}
--- pages/api/webhook.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  api: { bodyParser: false }
-};
-
-import Stripe from 'stripe';
-
-export default async function handler(req, res) {
-  res.status(200).end(); // placeholder webhook
-}
