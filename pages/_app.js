013ab01b5210d7707799796dbb0a7d68d7d2f4ae
dantheman88 <dannymcmullan6@gmail.com>
2025-12-12 01:12:52 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
-// pages/_app.js
 import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <nav className="top-nav">
+        <div className="nav-left">
+          <Link href="/">Early App</Link>
+        </div>
+
+        <div className="nav-right">
+          <Link href="/drops">Drops</Link>
+          <Link href="/profile">Profile</Link>
+        </div>
+      </nav>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
