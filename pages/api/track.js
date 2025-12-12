66960c82efb731de2f8e1dbbdae3a1bcd16881e5
dantheman88 <dannymcmullan6@gmail.com>
2025-12-12 21:08:17 +0000

    Update track.js

--- pages/api/track.js
+++ pages/api/track.js
@@ -2,7 +2,18 @@ export default function handler(req, res) {
   const { offerId } = req.query;
 
   const offerLinks = {
-    1: "https://www.awin1.com/cread.php?awinmid=87091&awinaffid=2693548", // ISOQAR
-    2: "https://www.awin1.com/cread.php?awinmid=1936&awinaffid=2693548",  // Theatre Tickets
-    // 3: broadband later (once approved)
+    1: "https://www.awin1.com/cread.php?awinmid=87091&awinaffid=2693548",
+    2: "https://www.awin1.com/cread.php?awinmid=1936&awinaffid=2693548",
+    3: "https://www.awin1.com/cread.php?awinmid=1936&awinaffid=2693548",
   };
+
+  const redirectUrl = offerLinks[offerId];
+
+  if (!redirectUrl) {
+    res.status(400).send("Invalid offer");
+    return;
+  }
+
+  res.writeHead(302, { Location: redirectUrl });
+  res.end();
+}
