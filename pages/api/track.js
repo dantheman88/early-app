9df37035a862d1371bcb93429dc149ef69b62559
dantheman88 <dannymcmullan6@gmail.com>
2025-12-12 17:32:15 +0000

    Update track.js

--- pages/api/track.js
+++ pages/api/track.js
@@ -2,7 +2,8 @@ export default function handler(req, res) {
   const { offerId } = req.query;
 
   const offerLinks = {
-    1: "https://www.awin1.com/cread.php?awinmid=870918&awinaffid=2693548",
+    1: "https://www.awin1.com/cread.php?awinmid=87091&awinaffid=2693548", // ISOQAR
+    2: "https://www.awin1.com/cread.php?awinmid=1936&awinaffid=2693548", // Theatre Tickets Direct
   };
 
   const redirectUrl = offerLinks[offerId];
